faabea66ed6914c386e256370a728af0e6196d13
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2021-03-28 16:59:19 +0300

    added EOchar class

---
+++ src/main/java/org/eolang/EOchar.java
@@ -0,0 +1,25 @@
+package org.eolang;
+
+import org.eolang.core.EOObject;
+import org.eolang.core.data.EOData;
+
+public class EOchar extends EOObject {
+    private char character;
+
+    public EOchar(char character){
+        this.character = character;
+    }
+
+    @Override
+    public EOData _getData() {
+        return new EOData(character);
+    }
+
+    /***
+     * Converts this character to a string
+     * @return An object representing the string type of this character
+     */
+    public EOstring EOtoString() {
+        return new EOstring(Character.toString(character));
+    }
+}
