001bac0b56db29f60fc363f56e44078c3d3aa396
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2021-03-29 19:32:17 +0300

    extracted a class for the cramers rule

--- src/main/java/MainCramers.java
+++ src/main/java/MainCramers.java
@@ -1,3 +1,4 @@
+import eo.test.EOCramers;
 import org.eolang.core.EOObject;
 import org.eolang.core.data.EODataObject;
 import org.eolang.io.EOstdout;
@@ -5,89 +6,45 @@ import org.eolang.txt.EOsprintf;
 import org.eolang.EOfloat;
 
 public class MainCramers {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-//        cramer's rule for solving 2 equations
-//        ax1 + ax2 = a;
-//        bx1 + bx2 = b
-
+//        Example 1
 //        equation 1
         EOfloat ax1 = new EOfloat(5.0);
         EOfloat ax2 = new EOfloat(7.0);
         EOfloat a = new EOfloat(2.0);
-
 //        equation 2
-        EOfloat bx1 = new EOfloat(1.0);
+        EOfloat bx1 = new EOfloat(7.0);
         EOfloat bx2 = new EOfloat(3.0);
-        EOfloat b = new EOfloat(4.0);
-
-        //matrix diagonals
-        EOfloat deltaDiag1 = new EOfloat(
-                ax1.EOmul(
-                        bx2
-                )._getData().toFloat()
-        );
-        EOfloat deltaDiag2 = new EOfloat(
-                ax2.EOmul(
-                        bx1
-                )._getData().toFloat()
-        );
-
-        EOfloat delta1Diag1 = new EOfloat(
-                a.EOmul(
-                        bx2
-                )._getData().toFloat()
-        );
-        EOfloat delta1Diag2 = new EOfloat(
-                b.EOmul(
-                        ax2
-                )._getData().toFloat()
-        );
-
-        EOfloat delta2Diag1 = new EOfloat(
-                ax1.EOmul(
-                        b
-                )._getData().toFloat()
-        );
-        EOfloat delta2Diag2 = new EOfloat(
-                bx1.EOmul(
-                        a
-                )._getData().toFloat()
-        );
-
-//        determinants
-        EOfloat delta = new EOfloat(
-                deltaDiag1.EOsub(
-                        deltaDiag2)._getData().toFloat()
-        );
-        EOfloat delta1 = new EOfloat(
-                delta1Diag1.EOsub(
-                        delta1Diag2)._getData().toFloat()
-        );
-        EOfloat delta2 = new EOfloat(
-                delta2Diag1.EOsub(
-                        delta2Diag2)._getData().toFloat()
-        );
-
-//        solutions
-        EOfloat x1 = new EOfloat(
-                delta1.EOdiv(
-                        delta)._getData().toFloat()
-        );
-        EOfloat x2 = new EOfloat(
-                delta2.EOdiv(
-                        delta)._getData().toFloat()
-        );
+        EOfloat b = new EOfloat(5.0);
 
         EOObject stdout = new EOstdout(
                 new EOsprintf(
-                        new EOsprintf(
-                                new EODataObject("Solution x1 is %f\nSolution x2 is %f\n"),
-                                x1,
-                                x2
+                        new EODataObject(
+                                "Example #1\n %s\n" +
+                                "Example #2\n %s\n" +
+                                "Example #3\n %s\n"),
+                        new EOCramers(ax1, ax2, a, bx1, bx2, b),
+                        new EOCramers(
+                                new EOfloat(3.0),
+                                new EOfloat(9.0),
+                                new EOfloat(4.0),
+                                new EOfloat(6.0),
+                                new EOfloat(2.0),
+                                new EOfloat(1.0)
+                        ),
+                        new EOCramers(
+                                new EOfloat(3.0),
+                                new EOfloat(2.0),
+                                new EOfloat(1.0),
+                                new EOfloat(8.0),
+                                new EOfloat(4.0),
+                                new EOfloat(2.0)
                         )
+
                 )
         );
         stdout._getData();
+
     }
 }
