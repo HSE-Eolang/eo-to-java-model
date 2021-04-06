a8ccbc321c69472333f237354f2ead30e5636491
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2021-04-06 19:19:19 +0300

    refactored

--- src/main/java/org/eolang/core/EOObject.java
+++ src/main/java/org/eolang/core/EOObject.java
@@ -2,11 +2,7 @@ package org.eolang.core;
 
 import org.eolang.core.data.EOData;
 import org.eolang.core.data.EODataObject;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.Optional;
 
 
@@ -65,39 +61,9 @@ public abstract class EOObject implements Cloneable{
      * @return a copy of the object
      */
     public EOObject _clone() {
-        /*try{
-            EOObject res  = (EOObject)this.clone();
-            for (Field field : this.getClass().getDeclaredFields()) {
-                field.setAccessible(true);
-                EOObject value = (EOObject)field.get(this);
-                //field.set(this, value._clone());
-                field.set(this, value);
-            }
-            return res;
-        }catch (CloneNotSupportedException cnsException){
-            cnsException.printStackTrace();
-        }catch (IllegalAccessException iaException){
-            iaException.printStackTrace();
-        }
-        return new EODataObject();*/
         return this;
 
     }
-
-    /**
-     * Assigning the object's attributes to null for later removal of the attributes by the garbage collector.
-     */
-    /*public void _freeAttributes(){
-        for (Field field : this.getClass().getDeclaredFields()) {
-            field.setAccessible(true);
-            try {
-                field.set(this, null);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-
-        }
-    }*/
 
     /**
      * Copying an attribute of an object by name.
@@ -105,36 +71,15 @@ public abstract class EOObject implements Cloneable{
      * @param name Object name
      * @return Object Attribute
      */
-//    public EOObject _getAttribute(String name) {
-//        EOObject res = new EODataObject();
-//        try{
-//            for (Field field : this.getClass().getDeclaredFields()) {
-//                if (field.getName().equals(name)) {
-//                    field.setAccessible(true);
-//                    EOObject value = (EOObject) field.get(this);
-//                    return value._clone();
-//                }
-//
-//            }
-//        }catch (IllegalAccessException iaException){}
-//        try{
-//            Class<?> attClasss = Class.forName(this.getClass().getName()+"$EO"+name);
-//            Constructor<?> attConstructor = attClasss.getConstructor();
-//            return ((EOObject)attConstructor.newInstance())._setParent(this);
-//        } catch (Exception e) {}
-//        if(_parent != null){
-//            res = _parent._getAttribute(name);
-//        }
-//        try{
-//            if(((EODataObject) res).isNoData()){
-//                Class<?> attClasss = Class.forName(this.getClass().getPackage().getName() +".EO"+name);
-//                Constructor<?> attConstructor = attClasss.getConstructor();
-//                return (EOObject)attConstructor.newInstance();
-//            }
-//
-//        } catch (Exception e) {}
-//        return res;
-//    }
+    public EOObject _getAttribute(String name){
+        EOObject res = new EODataObject();
+        try {
+            Method method = this.getClass().getDeclaredMethod("EO"+ name);
+            method.setAccessible(true);
+            return (EOObject)method.invoke(this);
+        } catch (Exception e) {}
+        return res;
+    }
 
     /**
      * Copying an attribute of an object by name with the installation of free attributes
@@ -143,33 +88,6 @@ public abstract class EOObject implements Cloneable{
      * @Param freeAtt Available attributes
      * @return Object Attribute
      */
-//    public EOObject _getAttribute(String name, EOObject... freeAtt) {
-//        EOObject res = new EODataObject();
-//        try {
-//            String className = this.getClass().getName()+"$EO"+name;
-//            Class<?> attClasss = Class.forName(className);;
-//            Constructor<?> attConstructor = Arrays.stream(attClasss.getConstructors())
-//                    .filter(constructor -> constructor.getParameterTypes().length == freeAtt.length)
-//                    .findFirst().get();
-//            return (EOObject)attConstructor.newInstance((Object[])freeAtt);
-//        } catch (Exception e) {}
-//        if(_parent != null){
-//            res = _parent._getAttribute(name, (EOObject[])freeAtt);
-//        }
-//        try{
-//            if(((EODataObject) res).isNoData()){
-//                String className = this.getClass().getPackage().getName() +".EO"+name;
-//                Class<?> attClasss = Class.forName(className);
-//                Constructor<?> attConstructor = Arrays.stream(attClasss.getConstructors())
-//                        .filter(constructor -> constructor.getParameterTypes().length == freeAtt.length)
-//                        .findFirst().get();
-//                return (EOObject)attConstructor.newInstance((Object[])freeAtt);
-//            }
-//
-//        } catch (Exception e) {}
-//        return res;
-//    }
-
     public EOObject _getAttribute(String name, EOObject... freeAtt){
         EOObject res = new EODataObject();
         try {
@@ -177,22 +95,7 @@ public abstract class EOObject implements Cloneable{
             method.setAccessible(true);
             return (EOObject) method.invoke(this, freeAtt);
         } catch (Exception e) {}
-        if(_parent != null){
-            res = _parent._getAttribute(name, freeAtt);
-        }
         return res;
     }
 
-    public EOObject _getAttribute(String name){
-        EOObject res = new EODataObject();
-        try {
-            Method method = this.getClass().getDeclaredMethod("EO"+ name);
-            method.setAccessible(true);
-            return (EOObject)method.invoke(this);
-        } catch (Exception e) {}
-        if(_parent != null){
-            return _parent._getAttribute(name);
-        }
-        return res;
-    }
 }
