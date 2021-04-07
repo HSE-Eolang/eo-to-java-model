2000f753e27a8b039bda7552cd32fe64f8428a0f
nlchar <contact.me@nlchar.rocks>
2021-04-07 15:11:39 +0300

    fix

--- src/main/java/transpiler/medium2target/Medium2TargetTranspiler.java
+++ src/main/java/transpiler/medium2target/Medium2TargetTranspiler.java
@@ -4,11 +4,10 @@ import transpiler.mediumcodemodel.EOSourceFile;
 import transpiler.mediumcodemodel.EOTargetFile;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 public class Medium2TargetTranspiler {
     public static void transpile(EOSourceFile file) {
-        Optional<ArrayList<EOTargetFile>> result = file.transpile(null);
+        ArrayList<EOTargetFile> result = file.transpile(null);
         return;
     }
 }
